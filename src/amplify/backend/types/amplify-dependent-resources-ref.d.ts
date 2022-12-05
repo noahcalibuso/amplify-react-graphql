1bd97b31bc19d65ccc5f25967909d9deb0bf43b2
noahcalibuso <noahcalibuso@gmail.com>
2022-12-05 13:59:23 -0800

    added auth

---
+++ src/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyreactgraphqlb5b7d2f7": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
